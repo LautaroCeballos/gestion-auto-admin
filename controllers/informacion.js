9ea2e1ff33fc110bc67d8dab1f6009a42796a2b5
Lautaro <lautaroceballos2013@gmail.com>
2021-03-30 14:59:30 -0300

    Mejoras de legibilidad y falcilidad de lectura

--- controllers/informacion.js
+++ controllers/informacion.js
@@ -4,9 +4,6 @@ const validator = require('validator');
 const Informacion = require('../models/informacion');
 
 const controller = {
-    metodoDePuerba: (req, res) => {
-        console.log("Este es el controlador de Informacion");
-    },
 
     save: (req, res) => {
         // Recoger parametros por post
--- routes/informacion.js
+++ routes/informacion.js
@@ -5,8 +5,6 @@ const InformacionController = require('../controllers/informacion');
 
 const router = express.Router();
 
-router.get('/info-pruebas', InformacionController.metodoDePuerba);
-
 router.post('/saveInfo', InformacionController.save);
 router.get('/getInfo', InformacionController.getInfo);
 router.put('/updateInfo/:id', InformacionController.update);
